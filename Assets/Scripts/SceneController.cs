a8b4e3c6d83dac4390be6854e02e824b308e547f
CastleBomber <richie.ucsb@gmail.com>
2020-09-16 20:56:57 -0700

    5.3.3

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private MemoryCard originalCard;
+    [SerializeField] private Sprite[] images;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int id = Random.Range(0, images.Length);
+        originalCard.SetCard(id, images[id]);
+    }
+}
